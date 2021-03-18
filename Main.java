63343fd10eb206349d1a9fb4277e9991ad21f060
faisalfirmansyah14 <80902541+faisalfirmansyah14@users.noreply.github.com>
2021-03-18 20:54:13 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+package GaltoLit;
+import java.util.Scanner;
+
+class GaltoLit1{
+
+    public static void main(String[] args){
+
+        int gallons = 0 ;
+        double liters;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Inputkan Gallon yang ingin di convert : ");
+        gallons = scan.nextInt();
+
+
+        liters = gallons *3.785;
+
+
+
+        System.out.println(gallons + " gallons adalah " + liters + " liters.");
+
+
+
+    }
+}
